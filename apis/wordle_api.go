8c09e141e7b85c59db7334b60e3d177f74d4ce3a
qiuzijie <771447612@qq.com>
2022-07-08 17:47:41 +0800

    tmp save

--- apis/wordle_api.go
+++ apis/wordle_api.go
@@ -22,7 +22,10 @@ var dayIndex int = 0
 func (api WordleApi) GetAllWords(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
 	result := db.QueryAllWords()
 	w.Header().Add("Content-Type", "application/json")
-	api.SetupCORS(&w)
+	(w).Header().Set("Access-Control-Allow-Origin", "*")
+	(w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	//api.SetupCORS(&w)
 	res, _ := json.Marshal(result)
 	resStr := string(res)
 	fmt.Println(w.Header().Get("Access-Control-Allow-Origin"))
